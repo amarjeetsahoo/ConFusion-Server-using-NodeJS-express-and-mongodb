2d419320342958e39c36e019ce4361929c8845c0
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2020-12-05 14:38:54 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
